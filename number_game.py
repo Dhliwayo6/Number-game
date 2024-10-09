bcc8e2cf10b11d2a135009182b2e73f514d45bf0
Taropafadzwa Chitumwa <mandisii@icloud.com>
2024-10-09 09:22:19 +0200

    Updated version

--- number_game.py
+++ number_game.py
@@ -13,33 +13,49 @@ def divide(a, b):
     if b != 0:
         return a / b
     else: 
-        print('Cannot dic=vide by 0!')
+        print('Cannot divide by 0!')
     
+def numbers():
+    num1 = randint(1,10)
+    num2 = randint(1,10)
 
-def randomNumber():
-    random_number = randint(1, 100)
-    random1 = random_number
-    random_num2 = randint(1,100)
-    random2 = random_num2
+    return num1, num2
 
-    return random1, random2
+operations = {
+    '+': add,
+    '-': subtract,
+    '*': multiply,
+    '/': divide
+}
 
-def calculation(num1, num2):
-
-    pass
-
-playing = True
-
-while playing:
+op = input('What do you want to do? Choose one operation +,-,*,/: ')
+operators = ['+', '-', '*', '/']
+while op not in operators:
+    print('Invalid option, please choose from one of the options +,-,*,/ ')
     op = input('What do you want to do? Choose one operation +,-,*,/: ')
-    operators = ['+', '-', '*', '/']
-    while op not in operators:
-        print('Invalid option, please choose from one of the options +,-,*,/ ')
-        op = input('What do you want to do? Choose one operation +,-,*,/: ')
+score = 0   
+if op in operations:
+    for _ in range(5):
+        num1, num2 = numbers()
+        result = operations[op](num1, num2)
+        reply = input(f'Calculate {num1} {op} {num2} = ')
 
-    if op == '+':
+    try:
+        answer = float(reply)
+        if answer == result:
+            print('Correct')
+            score += 1
+        else: 
+            print(f'Incorrect, the answer was {result}')
+            
+    except ValueError:
+        print('That was not a number, try again!')
+
+    print(f'Your got {score} questions correct')
+    print(f'You got {((score/5) * 100)} %')
 
 
+         
+        
 
     
-
